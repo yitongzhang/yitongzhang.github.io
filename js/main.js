f9e41ff8121b6a7c93f32cb3d192eb69479313df
Yitong <zha.yitong@gmail.com>
2017-09-20 01:23:00 -0700

    Type update

--- js/main.js
+++ js/main.js
@@ -11,3 +11,14 @@ function	hoverOutDot(){
 }
 
 $( '.dots li' ).mouseenter( hoverInDot ).mouseleave( hoverOutDot );
+
+$(window).scroll(function() {
+  var scrollPosition = $( "body" ).scrollTop();
+  if (scrollPosition == 0) {
+  	$( "nav" ).removeClass( 'sticky' )
+  }
+  else	{
+  	$( "nav" ).addClass( 'sticky' )
+  }
+
+});
