4086bd90b87cadfda45478c2bcbedab1090ce8ef
Yitong <zha.yitong@gmail.com>
2017-11-30 15:36:16 -0800

    last minute fixes

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(window).scroll(function() {
 
 });
 
-$("#workNavLi").find('a').click(function(e) {
+$("#index #workNavLi").find('a').click(function(e) {
     e.preventDefault();
     var section = $(this).attr("href");
     $("html, body").animate({
